aca44042050159975b9ccf3264696fa2f8756e04
Bhoomi <bhoomiahir321@gmail.conm>
2025-02-01 20:37:07 +0530

    Add project files.

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class StudentController : Controller
+    {
+        Student objStudent = new Student();
+
+        public IActionResult Index()
+        {
+            objStudent = new Student();
+            List<Student> lst = objStudent.getData("");
+            return View(lst);
+        }
+
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddStudent(Student emp)
+        {
+            bool res;
+            if (ModelState.IsValid)
+            {
+                res = objStudent.insert(emp);
+                if (res)
+                {
+                    TempData["msg"] = "Added successfully";
+                }
+                else
+                {
+                    TempData["msg"] = "Not Added. something went wrong..!!";
+                }
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditStudent(string id)
+        {
+            List<Student> emp = objStudent.getData(id);
+            return View(emp.FirstOrDefault());
+        }
+
+        [HttpGet]
+        public IActionResult DeleteStudent(string id)
+        {
+            List<Student> emp = objStudent.getData(id);
+            return View(emp.FirstOrDefault());
+        }
+
+        [HttpPost]
+        public IActionResult EditStudent(Student emp)
+        {
+            bool res;
+            if (ModelState.IsValid)
+            {
+                res = objStudent.update(emp);
+                if (res)
+                {
+                    TempData["msg"] = "Updated successfully";
+                }
+                else
+                {
+                    TempData["msg"] = "Not Updated. something went wrong..!!";
+                }
+            }
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult DeleteStudent(Student emp)
+        {
+            bool res;
+            if (ModelState.IsValid)
+            {
+                res = objStudent.delete(emp);
+                if (res)
+                {
+                    TempData["msg"] = "Deleted successfully";
+                }
+                else
+                {
+                    TempData["msg"] = "Not Deleted. something went wrong..!!";
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Age { get; set; }
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Bhoomi Dangar\\Downloads\\Student_NEW\\Student_NEW\\WebApplication2\\AppData\\Student_Demo.mdf;Integrated Security=True");
+
+        public List<Student> getData(string id)
+        {
+            List<Student> lstStu = new List<Student>();
+            string query = "select * from StudentData";
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                query = "select * from StudentData where Id =" + id;
+            }
+            SqlDataAdapter apt = new SqlDataAdapter(query, con);
+            DataSet ds = new DataSet();
+            apt.Fill(ds);
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                lstStu.Add(new Student { Id = Convert.ToInt32(dr["Id"].ToString()), Name = dr["Name"].ToString(), Email = dr["Email"].ToString(), Address = dr["Address"].ToString(), Age = dr["Age"].ToString() });
+            }
+            return lstStu;
+        }
+
+        //Insert a record into a database table
+        public bool insert(Student Stu)
+        {
+            if (Stu.Name != string.Empty && Stu.Email != string.Empty && Stu.Address != string.Empty)
+            {
+                SqlCommand cmd = new SqlCommand("insert into StudentData values(@name,@email,@address,@age)", con);
+                cmd.Parameters.AddWithValue("@name", Stu.Name);
+                cmd.Parameters.AddWithValue("@email", Stu.Email);
+                cmd.Parameters.AddWithValue("@address", Stu.Address);
+                cmd.Parameters.AddWithValue("@age", Stu.Age);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i >= 1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        //Update a record into a database table
+        public bool update(Student Emp)
+        {
+            SqlCommand cmd = new SqlCommand("update StudentData set Name=@name, Email=@email,Address=@Address,Age=@age where Id = @id", con);
+            cmd.Parameters.AddWithValue("@name", Emp.Name);
+            cmd.Parameters.AddWithValue("@email", Emp.Email);
+            cmd.Parameters.AddWithValue("@Address", Emp.Address);
+            cmd.Parameters.AddWithValue("@age", Emp.Age);
+            cmd.Parameters.AddWithValue("@id", Emp.Id); con.Open();
+            int i = cmd.ExecuteNonQuery();
+            if (i >= 1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //delete a record from a database table
+
+        public bool delete(Student Emp)
+        {
+            SqlCommand cmd = new SqlCommand("delete StudentData where Id = @id", con); 
+            cmd.Parameters.AddWithValue("@id", Emp.Id);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            if (i >= 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
